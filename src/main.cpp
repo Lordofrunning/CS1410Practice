7f61728940e43cdbcbcac52350d10309d4c7624c
TTChippy <61674508+Lordofrunning@users.noreply.github.com>
2026-01-21 04:08:14 +0000

    odd even to twenty practice code 0201.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
     // TODO
     std::cout << "Full Name: Tyler Chipman" << std::endl;
     std::cout << "git hub name: LordofRunning" << std::endl; 
     
+     for(int i = 1; i < 20; i += 2){
+        int j = i;
+        cout << i << "     " << (j + 1) << endl;
+
+        
+    }
+
     return 0;
 }
