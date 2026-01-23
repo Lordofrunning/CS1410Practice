e16ca51cc98ba06000d8f837112fef19385dfa34
TTChippy <61674508+Lordofrunning@users.noreply.github.com>
2026-01-23 02:22:52 +0000

    fixed code to also check for negative and zero numbers inputed by user.

--- src/asign01.cpp
+++ src/asign01.cpp
@@ -6,11 +6,12 @@ using namespace std;
 int main(){
     int count = 0;
     int numbers[5];
+    // start while loop to get input from user.
     while(count < 5){
         int num;
         cout << "give a number divisible by 3:" << endl;
         cin >> num;
-        if(num % 3 == 0){
+        if(num % 3 == 0 || num > 0){
             cout << num << " added to collection" << endl;
             numbers[count] = num;
             count++; 
@@ -20,6 +21,7 @@ int main(){
         }
 
     }
+    // get the minval of the numbers. 
     int minvalue = numbers[0];
     for(int i = 0; i < 5; i++){
         if(minvalue > numbers[i]){
@@ -27,6 +29,7 @@ int main(){
         }
         
     }
+    // get max val
     int maxvalue = numbers[0];
     for(int i = 0; i < 5; i++){
         if(maxvalue < numbers[i]){
@@ -34,16 +37,18 @@ int main(){
         }
         
     }
+    // get total 
     int total = 0;
     for(int i = 0; i < 5; i++){
         total += numbers[i];
     }
+    // get average. 
     int average = 0;
     for (int i = 0; i < 5; i++){
         average += numbers[i];
     }
     int realavg = average / 5;
-
+    // display the stuff. 
     cout << "count" << setw(10)  << "minimum" << setw(10) << "max" << setw(10) << "total" << setw(10) << "average" << endl;
     cout << count << setw(10) << minvalue << setw(10) << maxvalue << setw(10) << total << setw(10) << realavg << endl;
 
