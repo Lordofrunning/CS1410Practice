52a12791cd1b03da784af07b249b8a980eba0ebd
TTChippy <61674508+Lordofrunning@users.noreply.github.com>
2026-02-10 19:35:39 +0000

    code put in for ex0501, displaying months and days in each month.

---
+++ src/ex0501.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include <string> 
+#include <iomanip>
+
+using namespace std; 
+
+int main(){
+    string months[12] {
+        "January", "February", "March", "April", "May", "June", "july", "August", "September", "October", "November", "December"
+    };
+
+    int year;
+    cout << "Enter the current year: ";
+    cin >> year;
+
+    int days[]{
+        31, year % 4 == 0 ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    };
+
+    cout << setw(10) << "Month" << setw(12) << "# of days" << endl;
+    for(int i = 0; i <12; i++){
+        cout << setw(10) << months[i] << setw(12) << days[i] << endl;
+    }
+
+    return 0;
+}
