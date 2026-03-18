7ec94b116df25c2378c744396a5b13f3f6800009
TTChippy <61674508+Lordofrunning@users.noreply.github.com>
2026-03-18 03:13:36 +0000

    example 8.1 code.

---
+++ src/ex0801.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std; 
+
+class Author{
+private:
+    string name, nationality;
+
+public:
+    Author(const string& name, const string& nationality)
+     : name(name), nationality(nationality){}
+
+     string getName() const {return name;}
+     string getNationality() const {return nationality;}
+
+    void setName(const string& name){this->name = name;}
+    void setNationality(const string& nationality){this->nationality = nationality;}
+
+};
+
+class Book {
+private:
+    string title;
+    int year;
+    Author author; 
+
+public:
+    Book(const string& t, int y, const Author& a):title(t), year(y), author(a){    } 
+
+    string getTtile() const {return title;}
+    int getYear() const {return year;}
+    Author getAuthor() const {return author;}
+
+    void setTitle(const string& title){this->title = title;}
+    void setYear( int year){this->year = year;}
+    void setAuthor(const Author& a) {author = a;}
+
+    void show() const{
+        cout << "Book: " << title << "  " << year << "\n" <<
+            author.getName() << "   " << author.getNationality() << endl; 
+    }
+
+};
+
+int main(){
+
+    Author a("gorege orwell", "british");
+    Book b ("1984", 1949, a);
+
+    b.show(); 
+
+    return 0 ;
+}
